5f61e88b4c309ffd13222aaa16fe54de854bd328
Mezea11 <christiancastellanosmeza@gmail.com>
2024-02-07 13:41:52 +0100

    .

--- client/pages/home.js
+++ client/pages/home.js
@@ -2,7 +2,8 @@ export default function home() {
      return `
      <article id="home">
         <h1>Welcome to my home page</h1>
-        <p>This is the home page</p>
+        <p>This is the home page</p>'¨
+        
      </article>
      `
 }
