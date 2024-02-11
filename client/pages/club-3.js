f9e3073ebae0cce110ba4aa96ede3ba737cb72ca
linnclaesson <linn.denise.claesson@gmail.com>
2024-02-12 00:10:28 +0100

    updated conatiner id

--- client/pages/club-3.js
+++ client/pages/club-3.js
@@ -1,8 +1,8 @@
 export default function club3() {
     return `
-        <div id="container">
+        <div id="club-3-container">
             <h1>Welcome to my CLUB3 page</h1>
             <p class: "club3-p">This is the CLUB3 page</p> 
         </div>
-    `
+    `;
 }
