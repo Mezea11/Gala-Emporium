d70bbfb80b45c1ef170367ae5b4df9b87631ba53
PelleHanspers <p.hanspers@gmail.com>
2024-02-12 12:57:35 +0100

    small change in club.js

--- server/api/Clubs.js
+++ server/api/Clubs.js
@@ -1,16 +1,17 @@
 import mongoose from "mongoose";
-import admin from "./Admins.js"; // Assuming Admins.js exports the Admin model
+// import admins from "./Admins.js";
+// Assuming Admins.js exports the Admin model
 
 const clubsSchema = mongoose.Schema({
   club_name: String,
-  adminId: { type: mongoose.Schema.Types.ObjectId, ref: "Admin" } // Reference Admin model here
+  adminId: { type: mongoose.Schema.Types.ObjectId, ref: "Admins" } // Reference Admin model here
 });
 
 const Club = mongoose.model('clubs', clubsSchema);
 
 export default function clubs(server) {
   server.get('/api/clubs', async (req, res) => {
-    const clubs = await Club.find().populate('adminId');
+    const clubs = await Club.find();
     res.json(clubs);
   });
 
