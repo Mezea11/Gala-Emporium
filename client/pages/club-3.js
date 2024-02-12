f1c8fcb83680aba8fa546774e678f64673a6e834
linnclaesson <linn.denise.claesson@gmail.com>
2024-02-12 10:44:40 +0100

    structure of contents added club 3

--- client/pages/club-3.js
+++ client/pages/club-3.js
@@ -1,8 +1,49 @@
 export default function club3() {
     return `
         <div id="club-3-container">
-            <h1>Welcome to my CLUB3 page</h1>
-            <p class: "club3-p">This is the CLUB3 page</p> 
+            
+            <section id="about-club3">
+                <h1>Malmö Up Club</h1>
+                <p class: "club3-p">This is the CLUB3 page</p> 
+            </section>
+            
+            <section id="calender-club3">
+                <h1>Club Calender</h1>
+
+                <section class="event-club3">
+                    <article>
+                        <h2>Event  title</h2>
+                        <p>Event info</p>
+                    </article>
+                    <div>
+                        <img>
+                        <a><button>Book here</button></a>
+                    </div>
+                </section>
+
+                <section class="event-club3">
+                    <article>
+                        <h2>Event  title</h2>
+                        <p>Event info</p>
+                    </article>
+                    <div>
+                        <img>
+                        <a><button>Book here</button></a>
+                    </div>
+                </section>
+
+                <section class="event-club3">
+                    <article>
+                        <h2>Event  title</h2>
+                        <p>Event info</p>
+                    </article>
+                    <div>
+                        <img>
+                        <a><button>Book here</button></a>
+                    </div>
+                </section>
+
+            </section>
         </div>
     `;
 }
