af66f08ea30362ac23eb63bf19013e230f82e3ec
PelleHanspers <p.hanspers@gmail.com>
2024-02-12 12:49:09 +0100

    added initial html and css for club-4

--- client/pages/club-4.js
+++ client/pages/club-4.js
@@ -1,8 +1,44 @@
 export default function club4() {
     return `
-    <div id="container">
-        <h1>Welcome to my CLUB4 page</h1>
-        <p class: "club4-p">This is the CLUB4 page</p> 
+    <div id="club-4-container">
+        <div id="video-container">
+                <video autoplay muted loop id="video-background">
+                    <source src="./assets/club-4.mp4" type="video/mp4">
+                    Your browser does not support the video tag.
+                </video>
+            </div>
+        <section id="club-4-about">    
+            <h1>Blind Melon</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </section>
+        <section id="event-container-club-4">
+            <div class="event-club-4">
+                <span> 2024-04-15 20:00 </span>
+                <h2> Event Title </h2>
+                <p>Event description</p>
+                <button>Tickets</button>
+            </div>
+            <div class="event-club-4">
+                <span> 2024-04-15 20:00 </span>
+                <h2> Event Title </h2>
+                <p>Event description</p>
+                <button>Tickets</button>
+            </div>
+            
+            <div class="event-club-4">
+                <span> 2024-04-15 20:00 </span>
+                <h2> Event Title </h2>
+                <p>Event description</p>
+                <button>Tickets</button>
+            </div>
+            
+            <div class="event-club-4">
+                <span> 2024-04-15 20:00 </span>
+                <h2> Event Title </h2>
+                <p>Event description</p>
+                <button>Tickets</button>
+            </div>
+        </section>
     </div>
     `
 }
