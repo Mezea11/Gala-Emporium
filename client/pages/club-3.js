93e9478de008c7ed4e2e78210422a8265b130f68
linnclaesson <linn.denise.claesson@gmail.com>
2024-02-13 14:24:16 +0100

    fetch and render events in club 3 +  update await in router

--- client/pages/club-3.js
+++ client/pages/club-3.js
@@ -1,4 +1,34 @@
-export default function club3() {
+export default async function club3() {
+    // -- fetch events based on clubId
+    // -- store in variable response
+    // -- save the response in json format in variable result
+    const response = await fetch('/api/events/65cb3564be0cac4cac4af614');
+    const result = await response.json();
+
+    // -- create variable which will be html element rendered in frontend containing fetched event data
+    let club3Event = '';
+
+    // loop through fetched result
+    for (let i = 0; i < result.length; i++) {
+        let data = result[i];
+
+        console.log(data);
+        // -- assign data to html elements
+        club3Event += `
+        <section class="event-club3">
+                    <article>
+                        <h3>${data.title}</h3>
+                        <p>${data.description}</p>
+                    </article>
+                    <div>
+                        <img>
+                        <a><button>Book here</button></a>
+                    </div>
+                </section>
+        
+        `;
+    }
+
     return `
         <div id="club-3-container">
             
@@ -9,7 +39,8 @@ export default function club3() {
             
             <section id="calender-club3">
                 <h2>Club Calender</h2>
-
+                ${club3Event}
+                
                 <section class="event-club3">
                     <article>
                         <h3>Event  title</h3>
