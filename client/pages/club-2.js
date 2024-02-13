1c8618eb4ad28ff79318fd7b3049be62072c634c
Erikm93 <erikaugust@hotmail.com>
2024-02-13 12:41:26 +0100

    Fixing spelling  and small change css

--- client/pages/club-2.js
+++ client/pages/club-2.js
@@ -3,7 +3,7 @@ export default function club2() {
     <div id="club-2-container"> 
         <section id="title-club2">
             <h1>Valhall</h1>
-            <h2>Live rock and metall at it's best</h2>
+            <h2>Live rock and metal at it's best</h2>
         </section>
         <div id="sidebar-club2">
             <p>One of Scandanavias biggest music clubs ever since it's inception in 1996. Bla bla</p>
