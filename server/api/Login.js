092d54d22ebe738508d7f55f84ceacc2966b39a0
PelleHanspers <p.hanspers@gmail.com>
2024-02-15 15:40:50 +0100

    testing session api for login

--- server/api/Login.js
+++ server/api/Login.js
@@ -1,7 +1,8 @@
 import { adminsModel } from './Admins.js';
 
 export default async function () {
-    server.post('/login', async (req, res) => {
+    /*
+    server.post('api/login', async (req, res) => {
         try {
             const check = await adminsModel.findOne({
                 username: req.body.username,
@@ -17,4 +18,23 @@ export default async function () {
             res.send('Wrong input. Please enter your username and password.');
         }
     });
+*/
+    server.post('/api/login', async (req, res) => {
+        if (req.session.login) {
+            res.json({ message: `Denna användare verkar redan vara inloggad.` });
+        } else {
+            const admin = await adminsModel.findOne({
+                username: req.body.username,
+                password: req.body.password
+            })
+
+            if (admin) {
+                req.session.login = user._id;
+                res.json({ mysession: req.session, message: `Välkommen ${user.username }. Du är nu inloggad` })
+                //res.json(user)
+            } else {
+                res.json({ message: 'kan inte hitta användare' });
+            }
+        }
+    })
 }
--- server/server.js
+++ server/server.js
@@ -1,35 +1,32 @@
 // IMPORT SERVER DEPENDENCIES
-import express from 'express';
-import mongoose from 'mongoose';
-import apiRegister from './api-register.js';
-//import session from "express-session"
+import express from "express"
+import mongoose from "mongoose"
+import apiRegister from "./api-register.js"
+import session from "express-session"
+
 
 // SERVER FUNCTION CALLED
-const server = express();
+const server = express()
 // PORT OPEN ON :80
-const port = 80;
+const port = 80
 
-server.use(express.json());
-server.use(express.static('../client'));
-/*server.use(
-    session({
-        secret: 'ditt_hemliga_tangent', // en hemlig nyckel för att signera session-cookie
-        resave: false, // undviker att spara sessionen om den inte ändras
-        saveUninitialized: true, // spara en ny session som inte har blivit initialiserad
-        cookie: { secure: false }, // cookie-inställningar, secure bör vara true i produktion med HTTPS
-    })
-);
-*/
+server.use(express.json())
+server.use(express.static('../client'))
+
+server.use(session({
+  secret: 'word', // en hemlig nyckel för att signera session-cookie
+  resave: false, // undviker att spara sessionen om den inte ändras
+  saveUninitialized: true, // spara en ny session som inte har blivit initialiserad
+  cookie: { secure: false} // cookie-inställningar, secure bör vara true i produktion med HTTPS
+}))
 
 // MONGODB CLUSTER CONNECTION
-mongoose.connect(
-    'mongodb+srv://christiancastellanosmeza:hello123@gala-emporium-2024.p4a4x4z.mongodb.net/emporium'
-);
+mongoose.connect("mongodb+srv://christiancastellanosmeza:hello123@gala-emporium-2024.p4a4x4z.mongodb.net/emporium")
 
 // API-REGISTER.JS FUNCTION CALLED
-apiRegister(server);
+apiRegister(server)
 
 //  LISTENING TO THE SERVER
 server.listen(port, () => {
-    console.log(`Server open on http://localhost:${port}`);
-});
+  console.log(`Server open on http://localhost:${port}`)
+})
