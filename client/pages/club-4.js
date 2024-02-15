fbad8396fa35e78bd93a696ae27378b8beb5d4ca
PelleHanspers <p.hanspers@gmail.com>
2024-02-15 12:20:59 +0100

    added assets and css update

--- client/pages/club-4.js
+++ client/pages/club-4.js
@@ -10,8 +10,10 @@ export default async function club4() {
         
         anything += `
         <div class="event-club-4">
+            <span> 2024-04-15 20:00 </span>
             <h1>${data.title}</h1>
             <p>${data.description}</p>
+            <button>Tickets</button>
         </div>
       `
 //      console.log(result);
@@ -27,7 +29,10 @@ export default async function club4() {
             </div>
         <section id="club-4-about">    
             <h1>Blind Melon</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>Blind Melon is the club celebrating the eclectic side of 90's rock music. A crossover in time when labels, 
+            even the smaller one, still had considerable resources while the media landscape was changing in a way that music 
+            could reach an audience, even if you weren't a top 3 artist on a top 3 label. What ensued was moment of movement, 
+            a excess of express, an emulsion of emotions and an emotional eon of awesome condensed to a diamond in our timeline.</p>
         </section>
 
         <section id="event-container-club-4">
