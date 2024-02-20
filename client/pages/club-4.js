db7bbe645162fffbbd597f74373479d020e10de7
PelleHanspers <p.hanspers@gmail.com>
2024-02-20 12:13:39 +0100

    css and event-container

--- client/pages/club-4.js
+++ client/pages/club-4.js
@@ -1,5 +1,5 @@
 export default async function club4() {
-    const response = await fetch('/api/events/65ca1005dd4d79add97d34c6') //clubId: 65ca1005dd4d79add97d34c6
+    const response = await fetch('/api/events/65ca1005dd4d79add97d34c6')
     const result = await response.json()
 
     let myEvents = ''
@@ -14,80 +14,46 @@ export default async function club4() {
         let timeString = date.getHours().toString().padStart(2, '0') + ':' + date.getMinutes().toString().padStart(2, '0');
         
         myEvents += `
-        <div class="event-club-4">
-            <span>${ dateString }</span>
-            <span>${ timeString }</span>
+        <article class="event-club-4">
+            <div class ="event-club-4-dateTime"
+                <span>Date: ${ dateString }</span>
+                <span> Time: ${ timeString }</span>
+            </div>
             <h1>${ data.title }</h1>
             <p>${ data.description }</p>
-            <button>Tickets</button>
-        </div>
+            <p>Available tickets: ${data.available_tickets}</p>
+            <button class="booking-button" onclick="navigateToBooking('${data._id}')">Tickets</button>
+        </article>
       `
 
     }
 
-      return `
-      <div id="club-4-container">
-        <div id="video-container">
+    return `
+        <div id="club-4-container">
+            <div id="video-container">
                 <video autoplay muted loop id="video-background">
                     <source src="./assets/club-4.mp4" type="video/mp4">
                     Your browser does not support the video tag.
                 </video>
             </div>
-        <section id="club-4-about">    
-            <h1>Blind Melon</h1>
-            <p>Blind Melon is the club celebrating the eclectic side of 90's rock music. A crossover in time when labels, 
-            even the smaller one, still had considerable resources while the media landscape was changing in a way that music 
-            could reach an audience, even if you weren't a top 3 artist on a top 3 label. What ensued was moment of movement, 
-            a excess of express, an emulsion of emotions and an emotional eon of awesome condensed to a diamond in our timeline.</p>
-        </section>
+            <section id="club-4-about">    
+                <h1>Blind Melon</h1>
+                <p>Blind Melon is the club celebrating the eclectic side of 90's rock music. A crossover in time when labels, 
+                even the smaller one, still had considerable resources while the media landscape was changing in a way that music 
+                could reach an audience, even if you weren't a top 3 artist on a top 3 label. What ensued was moment of movement, 
+                a excess of express, an emulsion of emotions and an emotional eon of awesome condensed to a diamond in our timeline.</p>
+            </section>
 
-        <section id="event-container-club-4">
-            ${myEvents}
-        </section>
+            <section id="event-container-club-4">
+                ${myEvents}
+            </section>
+        </div>
         `
     }
 
-/*    return `
-    <div id="club-4-container">
-        <div id="video-container">
-                <video autoplay muted loop id="video-background">
-                    <source src="./assets/club-4.mp4" type="video/mp4">
-                    Your browser does not support the video tag.
-                </video>
-            </div>
-        <section id="club-4-about">    
-            <h1>Blind Melon</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </section>
+    function navigateToBooking(eventId) {
+        window.location.href = "#booking";
+        window.bookingEventId = eventId;
+    }
 
-        <section id="event-container-club-4">
-            <div class="event-club-4">
-                <span> 2024-04-15 20:00 </span>
-                <h2> ${data.title} </h2>
-                <p> ${data.description} </p>
-                <button>Tickets</button>
-            </div>
-            <div class="event-club-4">
-                <span> 2024-04-15 20:00 </span>
-                <h2> Event Title </h2>
-                <p>Event description</p>
-                <button>Tickets</button>
-            </div>
-            
-            <div class="event-club-4">
-                <span> 2024-04-15 20:00 </span>
-                <h2> Event Title </h2>
-                <p>Event description</p>
-                <button>Tickets</button>
-            </div>
-            
-            <div class="event-club-4">
-                <span> 2024-04-15 20:00 </span>
-                <h2> Event Title </h2>
-                <p>Event description</p>
-                <button>Tickets</button>
-            </div>
-        </section>
-    </div>
-    `
-}*/
+    window.navigateToBooking = navigateToBooking;
